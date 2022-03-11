247d425448da09537f23902522634e76455f7969
lizzytag12 <luisa.h.morales@gmail.com>
2022-03-11 11:54:49 -0300

    cambios en home

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require("express")
 const res = require("express/lib/response")
 const app = express()
 const path = require("path")
-port =3030
+port = 3030;
+
+
 app.use(express.static("public"));
 
 
-app.get("/",(req,res) => res.sendFile(path.resolve(__dirname,"views","home.html")) )
+app.get("/",(req,res) => { return res.sendFile(path.resolve(__dirname,"views","home.html")) } )
 
 
-app.listen(port , ()=>{
-
-  return  console.log(`server running on port: htttp://localhost${port}`);
+app.listen(port, () => { // a traves de ese puerto van a entrar las peticiones asi que le digo que escuche.
+    console.log(`servidor corriendo en el puerto http://localhost:${port}`)
 })
